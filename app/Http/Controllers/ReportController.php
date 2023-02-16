c79c2a8ceb445b55b70b1999dfc095f433a7a2ce
EdisonR02 <riveraeddy58@gmail.com>
2023-02-15 19:30:33 -0500

    Fixed: Report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
-<<<<<<< HEAD
-
-=======
-use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
->>>>>>> 185fbc216e0c77bddcc1f968a3928b8ec7abb7d6
 class ReportController extends Controller
 {
     //
@@ -20,11 +15,7 @@ class ReportController extends Controller
     {
         //Busqueda de informacion
         $texto = trim($request->get('texto'));
-<<<<<<< HEAD
         $reportes = Informe::with('user')->get();
-=======
-        $reportes = informe::with('user')->get();
->>>>>>> 185fbc216e0c77bddcc1f968a3928b8ec7abb7d6
         return view ('report.index')->with('reportes',$reportes);
     }
 
@@ -45,11 +36,7 @@ class ReportController extends Controller
         //Campos a validar
         $campos=[
             'title'=>'required|string|max:50',
-<<<<<<< HEAD
             'information'=>'required|string|min:200',
-=======
-            'information'=>'required|string|max:200',
->>>>>>> 185fbc216e0c77bddcc1f968a3928b8ec7abb7d6
             'image'=>'required|max:1000|mimes:jpeg,png,svg,jpg',
         ];
 
@@ -61,7 +48,6 @@ class ReportController extends Controller
         ];
 
         $this->validate($request, $campos, $mensaje);
-<<<<<<< HEAD
         //Conseguir datos
         $datosReporte = $request-> except('_token');
         if($request -> hasFile('image')){
@@ -71,32 +57,6 @@ class ReportController extends Controller
 
         Informe::insert($datosReporte);
         return to_route('report')->with('mensaje','Registro Creado');
-=======
-        
-
-            // Obtener el archivo de imagen del formulario
-        $imagePath = $request->file('image')->getRealPath();
-
-        // Cargar la imagen en Cloudinary
-        $uploaded = Cloudinary::upload($imagePath, [
-            'folder' => 'my_folder',
-            'public_id' => 'my_image',
-        ]);
-
-        // Obtener la URL segura de la imagen cargada
-        $imageUrl = $uploaded->getSecurePath();
-
-        // Crear un nuevo producto y asignar la URL de la imagen cargada al campo de imagen_url
-        $informe = new Informe();
-        $informe->title = $request->title;
-        $informe->information = $request->information;
-        $informe->image= $imageUrl;
-        $informe->user_id = Auth::id();
-        $informe->save();
-
-        // Redirigir al usuario a la página del producto creado
-        return to_route('report')->with('mensaje','Registro Creado');
->>>>>>> 185fbc216e0c77bddcc1f968a3928b8ec7abb7d6
     }
 
 }
